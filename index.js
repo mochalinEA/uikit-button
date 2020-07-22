6f7d9d2767f8fd1a3581e464d0cc00bc36f9e6d8
Евгений Мочалин <em@utair.digital>
2020-07-22 11:56:11 +0300

    add children to button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const Button = (
-  <button>button</button>
-);
+export const Button = ({ children }) => {
+  return <button>{children}</button>
+};
 
 export default Button;
